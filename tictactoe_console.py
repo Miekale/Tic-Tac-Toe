d890478cc2022513b53f460eb0f4b04eb0cd0092
Miekale <miekale.smith@gmail.com>
2023-07-18 13:28:16 -0400

    working

--- tictactoe_console.py
+++ tictactoe_console.py
@@ -1,6 +1,6 @@
 import numpy as np
 import random
-
+import os
 #make board
 board = np.array([[" "," "," "], [" "," "," "], [" "," "," "]])
 current_player = "X"
@@ -24,19 +24,30 @@ def print_board(board):
     print(line)
     print(board[0, 0]+ "|" + board[0,1] + "|" + board[0,2])
 
+#clear output function
+def cls():
+    os.system('cls' if os.name=='nt' else 'clear')
+
 #choose which person goes first
 
 def start_player(player):
     if random.getrandbits(1) == False:
-        player = "X"
+        return "X"
     else:
-        player = "Y"
+        return "O"
 
 #player input
 def get_input(player):
     print(f'Which row and column to put the {player}')
-    x = int(input())
-    y = int(input())
+    x = input()
+    y = input()
+    try:
+        x = int(x)
+        y = int(y)
+    except:
+        get_input(player)
+    x = int(x)
+    y = int(y)
     if x > 3 or y > 3 or x < 1 or y < 1:
         print("Row or column out of range")
         get_input(player)
@@ -54,7 +65,9 @@ def modify_board(board, row, column, player):
 
 def is_winner(board, win_conditions):
     for solution in win_conditions:
-        if (board[solution[0,1]] == board[0 ,solution[1]] == board[0, solution[2]]):
+        board[solution[0]]
+        if (board[solution[0,0], solution[0,1]] == board[solution[1, 0], solution[1, 1]] == board[solution[2, 0], solution[2, 1]] 
+            and board[solution[0,0], solution[0,1]] != " "):
             return True
     return False
 
@@ -63,19 +76,27 @@ def is_winner(board, win_conditions):
 #Switch player
 def switch_player(player):
     if player == "X":
-        player = "O"
+        return "O"
     else:
-        player = "X"
+        return "X"
 
 #main program
 
-start_player(current_player)
+current_player = start_player(current_player)
 
 while winner == None:
+    cls()
+    
     print_board(board)
-    row, column = get_input(current_player)
+
+    column, row = get_input(current_player)
+
     modify_board(board, row, column, current_player)
+
     if is_winner(board, win_conditions):
         print(f'{current_player} Wins!!')
+        winner = "True"
     else:
-        switch_player(current_player)
+        current_player = switch_player(current_player)
+
+    
